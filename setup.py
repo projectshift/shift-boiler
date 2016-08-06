282712e160ef3e27ff8982751dc4b6d9095c9db3
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2016-08-06 13:51:43 +0100

    Bumping version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version='0.0.16'
+version='0.0.17'
 
 # development status
 # dev_status = '1 - Planning'
