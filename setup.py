e20fa135b4a2f0dbb11f4b6435a90197c60003f2
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2016-08-21 10:28:22 +0100

    Hotfix release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version='0.0.23'
+version='0.0.24'
 
 # development status
 # dev_status = '1 - Planning'
