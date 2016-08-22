73dc27d3d96b67f779bc26f75a407eaa994d43f8
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2016-08-22 08:36:33 +0100

    Fixing path to migration templates

--- boiler/migrations/config.py
+++ boiler/migrations/config.py
@@ -25,6 +25,6 @@ class MigrationsConfig(AlembicConfig):
         Get path to migrations templates
         This will get used when you run the db init command
         """
-        dir = os.path.join(os.getcwd(), 'boiler', 'migrations', 'templates')
+        dir = os.path.join(os.path.dirname(__file__), 'templates')
         return dir
 
