a382baa062cee65647e0e902df5f6c2a18c49981
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2016-12-04 10:53:53 +0000

    Release 0.0.27

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version='0.0.26'
+version='0.0.27'
 
 # development status
 # dev_status = '1 - Planning'
