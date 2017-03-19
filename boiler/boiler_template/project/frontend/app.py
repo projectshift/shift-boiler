5d9714ff6d077348e4395bdc685b86740c419616
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2017-03-19 10:04:13 +0000

    Minor formatting improvements

--- boiler/boiler_template/project/frontend/app.py
+++ boiler/boiler_template/project/frontend/app.py
@@ -8,17 +8,17 @@ def create_app(*args, **kwargs):
     # enable features
     bootstrap.add_routing(app)
     
-        # bootstrap.add_api(app)
-        # bootstrap.add_localization(app)
-        # bootstrap.add_routing(app)
-        # bootstrap.add_orm(app)
-        # bootstrap.add_logging(app)
-        # bootstrap.add_mail(app)
-        # bootstrap.add_jinja_extensions(app)
-        # bootstrap.add_error_pages(app)
+    # bootstrap.add_api(app)
+    # bootstrap.add_localization(app)
+    # bootstrap.add_routing(app)
+    # bootstrap.add_orm(app)
+    # bootstrap.add_logging(app)
+    # bootstrap.add_mail(app)
+    # bootstrap.add_jinja_extensions(app)
+    # bootstrap.add_error_pages(app)
     # bootstrap.add_navigation(app)
     # bootstrap.add_debug_toolbar(app)
     #
-        # bootstrap.add_users(app)
+    # bootstrap.add_users(app)
 
     return app
