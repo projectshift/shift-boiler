ea4dce453722fc5d5a9f9fdda7c8e80af4cd5e11
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2017-05-01 16:17:37 +0100

    Default behavior is to require confirmation

--- boiler/config/default_config.py
+++ boiler/config/default_config.py
@@ -146,7 +146,7 @@ class DefaultConfig(BaseConfig):
 
     # users
     USER_PUBLIC_PROFILES = True
-    USER_ACCOUNTS_REQUIRE_CONFIRMATION = False
+    USER_ACCOUNTS_REQUIRE_CONFIRMATION = True
 
     # templating
     TEMPLATES_FALLBACK_TO_KERNEL = True
