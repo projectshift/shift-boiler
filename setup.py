c0c43d7a69f7eb49865d7a5f577e0eec8cd15b9f
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2017-05-24 11:27:19 +0100

    Minor maintenance release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version='0.1.2'
+version='0.1.3'
 
 # development status
 # dev_status = '1 - Planning'
