adda7c3c82fbbc9a3e8e1443d8730efae4b8fd00
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2017-12-28 13:05:18 +0300

    JWT implementation prototype with tests

--- boiler/tests/user_tests/jwt_test.py
+++ boiler/tests/user_tests/jwt_test.py
@@ -2,8 +2,8 @@ from unittest import mock
 from nose.plugins.attrib import attr
 from boiler.tests.base_testcase import BoilerTestCase
 
-import jwt, time
-from boiler.user import user_token
+import jwt, datetime, calendar
+from boiler.user.user_token import UserToken
 
 
 @attr('user', 'token', 'jwt')
@@ -22,7 +22,7 @@ class JwtTests(BoilerTestCase):
     def test_generate_jwt(self):
         """ Generating JWT token"""
         user_id = 10
-        token = user_token.create_user_token(user_id, self.secret, self.algo)
+        token = UserToken().create_user_token(user_id, self.secret, self.algo)
         self.assertEquals(str, type(token))
         decoded = jwt.decode(token, self.secret, algorithms=[self.algo])
         self.assertEquals(user_id, decoded['user_id'])
@@ -30,16 +30,52 @@ class JwtTests(BoilerTestCase):
     def test_token_fails_if_tampered_with(self):
         """ Token fails if signature invalid"""
         user_id = 10
-        token = user_token.create_user_token(user_id, self.secret, self.algo)
+        token = UserToken().create_user_token(user_id, self.secret, self.algo)
         with self.assertRaises(jwt.exceptions.DecodeError):
             jwt.decode(token + 'x', self.secret, algorithms=[self.algo])
 
     def test_fail_to_decode_expired_token(self):
         """ Fail to decode expired user token """
         user_id = 10
-        token = user_token.create_user_token(user_id, self.secret, self.algo, -1)
+        token = UserToken().create_user_token(
+            user_id,
+            self.secret,
+            self.algo,
+            -1
+        )
         with self.assertRaises(jwt.exceptions.ExpiredSignatureError):
             jwt.decode(token, self.secret, algorithms=[self.algo])
+
+    def test_can_register_token_implementation(self):
+        """ Registering custom token implementation """
+
+        user_id = 1
+
+        @UserToken.token_implementation
+        def custom_token(user_id):
+            secret = self.secret
+            algo = self.algo
+            data = dict(user_id=user_id, something='else')
+            return jwt.encode(data, secret, algo)
+
+        token = UserToken().get_token(user_id)
+        self.assertEquals(custom_token(user_id), token)
+
+        # deregister
+        UserToken.custom_token_implementation = None
+
+    def test_fall_back_to_default_token_implementation(self):
+        """ Using default tokens if custom implementation not registered"""
+        user_id = 1
+        token = UserToken().get_token(user_id)
+        data = jwt.decode(token, self.secret, self.algo)
+        for claim in ['exp', 'nbf', 'iat', 'user_id']:
+            self.assertIn(claim, data.keys())
+
+
+
+
+
 
 
 
