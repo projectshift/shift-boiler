a54e2184aaa19e736bbfd8e84d6ee48a1d2f7ab0
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-01-02 19:18:32 +0300

    Bugfix release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version='0.2.0'
+version='0.2.1'
 
 # development status
 # dev_status = '1 - Planning'
