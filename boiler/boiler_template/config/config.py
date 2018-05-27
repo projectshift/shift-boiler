f8aba75e996fda1e76f9f0c858e3f3d92e253a1c
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-05-27 16:56:43 +0100

    Changing config inheritance a bit

--- boiler/boiler_template/config/config.py
+++ boiler/boiler_template/config/config.py
@@ -6,12 +6,12 @@ class DefaultConfig(config.DefaultConfig):
     pass
 
 
-class DevConfig(DefaultConfig, config.DevConfig):
+class DevConfig(config.DevConfig, DefaultConfig):
     """ Local development config """
     pass
 
 
-class TestingConfig(DefaultConfig, config.TestingConfig):
+class TestingConfig(config.TestingConfig, DefaultConfig):
     """ Local testing config """
     pass
 
