1e79c70e1174d220b2edbea9d809be11b864733a
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2019-07-02 12:39:17 +0100

    Adding a base config for options shared between environemnts

--- boiler/boiler_template/backend/config.py
+++ boiler/boiler_template/backend/config.py
@@ -2,12 +2,21 @@ from boiler import config
 import os
 
 
-class ProductionConfig(config.ProductionConfig):
+class BaseConfig(config.Config):
+    """
+    Base config
+    Use this to store configuration options that a shared among
+    environment-specific configs below.
+    """
+    pass
+
+
+class ProductionConfig(config.ProductionConfig, BaseConfig):
     """ Production config """
     pass
 
 
-class DevConfig(config.DevConfig):
+class DevConfig(config.DevConfig, BaseConfig):
     """ Local development config """
 
     # static assets
@@ -15,7 +24,7 @@ class DevConfig(config.DevConfig):
     FLASK_STATIC_PATH = os.path.realpath(os.getcwd() + '/web')
 
 
-class TestingConfig(config.TestingConfig):
+class TestingConfig(config.TestingConfig, BaseConfig):
     """ Local testing config """
     pass
 
